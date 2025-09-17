e945005cea886919727a1375450dbd9393cd74b1
Mohd Sadiq <mohd.sadiq@dotevolve.co.in>
2025-09-17 18:40:45 +0530

    Bump version code to 2 and version name to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.dotevolve.benchmark"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
