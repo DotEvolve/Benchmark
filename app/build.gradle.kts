933a3029bdcbe121f369bed26e677ff5a6b02ae6
Mohd Sadiq <mohd.sadiq@dotevolve.co.in>
2025-11-16 15:23:07 +0530

    feat: Bump version to 16
    Updates the application version code to 16 and the version name to "16".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "net.dotevolve.benchmark"
         minSdk = 23
         targetSdk = 36
-        versionCode = 15
-        versionName = "14.5"
+        versionCode = 16
+        versionName = "16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
